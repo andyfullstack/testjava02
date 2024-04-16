6ea80206ff2eaeecc1469b1ac8fe4ba63eeda67d
andyfullstack <andriikoliadenko@gmail.com>
2024-04-16 19:26:06 +0300

    commit 2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("16.04.24, 2");
+        System.out.println("Hello world!");
+        System.out.println("Nice to meet you!");
+    }
+}
